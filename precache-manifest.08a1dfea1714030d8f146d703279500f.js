e2b6bdd18e670f0e2e384aa10ebbc3fbc9622f04
William Pei Yuan <williampeiyuan@gmail.com>
2020-05-09 18:13:46 -0400

    Updates

---
+++ precache-manifest.08a1dfea1714030d8f146d703279500f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37879da9e6adfe382d899f3fde9c2e08",
+    "url": "/Articulate-code-challenge/index.html"
+  },
+  {
+    "revision": "b7d6f52d0d12caccb815",
+    "url": "/Articulate-code-challenge/static/css/main.2558d39d.chunk.css"
+  },
+  {
+    "revision": "004b9296bef1a84935c5",
+    "url": "/Articulate-code-challenge/static/js/2.be169b1c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Articulate-code-challenge/static/js/2.be169b1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7d6f52d0d12caccb815",
+    "url": "/Articulate-code-challenge/static/js/main.9fe7884e.chunk.js"
+  },
+  {
+    "revision": "279592f5260a873c1198",
+    "url": "/Articulate-code-challenge/static/js/runtime-main.66386419.js"
+  },
+  {
+    "revision": "9ae13e8bc2dcb89f2e23b2d6ad5d3109",
+    "url": "/Articulate-code-challenge/static/media/flipIcon.9ae13e8b.svg"
+  }
+]);
